17c5acd3a3210a1611afb80fa2ec08428f037fd5
Abdul-Samad-Feroz <abdulsamadferoz786@gmail.com>
2026-09-09 00:16:37 +0500

    Comparison Operators

---
+++ Operators/Comparison Operator/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int num1 = 10;
+    int num2 = 20;
+
+    printf("%i", num1 == num2);
+    printf("\n%i", num1 != num2);
+    printf("\n%i", num1 > num2);
+    printf("\n%i", num1 < num2);
+
+    return 0;
+}
